eb2739ac74d2c2b0ebadda77a7308a6c64436cfd
f-mac-mows <f.mac.mows@gmail.com>
2026-02-05 18:14:42 +0900

    readme.md 수정

--- .open-next/cloudflare/next-env.mjs
+++ .open-next/cloudflare/next-env.mjs
@@ -1,3 +1,3 @@
-export const production = {"NEXT_PUBLIC_API_URL":"https://walrung.com/api"};
-export const development = {"NEXT_PUBLIC_API_URL":"https://walrung.com/api"};
+export const production = {"NEXT_PUBLIC_API_URL":"https://api.walrung.com/api"};
+export const development = {"NEXT_PUBLIC_API_URL":"https://api.walrung.com/api"};
 export const test = {};
